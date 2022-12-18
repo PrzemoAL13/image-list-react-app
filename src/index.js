efe7db3ef6a83e82e7106368c87791dddfb87359
Przemek Tomaszewski <tomaszewski.przemyslaw1993@gmail.com>
2022-12-18 12:57:42 +0100

    new components created

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-
-const App = () => {
-	return <div>Hi there !</div>;
-};
+import App from "./components/App";
 
 createRoot(document.getElementById("root")).render(<App />);
