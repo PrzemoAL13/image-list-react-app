4d973ca018eb4ab2ed5cdc67614b2372d8ab10c9
Przemek Tomaszewski <tomaszewski.przemyslaw1993@gmail.com>
2022-12-19 07:09:20 +0100

    axios - to get data from API + async await function

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import axios from "axios";
 import SearchInput from "./SearchInput";
+
 class App extends React.Component {
-	onSearchSubmit = entry => {
-		console.log(entry);
-	};
+	async onSearchSubmit(entry) {
+		// console.log(entry);
+		const response = await axios.get(
+			`https://pixabay.com/api/?key=29305409-8468ce75218a81b211a88e2b7&q=${entry}&image_type=photo`
+		);
+		console.log(response);
+	}
 
 	render() {
 		return (
