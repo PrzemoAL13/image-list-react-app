2b371acfb7ccf8b8bff971f312f4fd72931776c3
Przemek Tomaszewski <tomaszewski.przemyslaw1993@gmail.com>
2022-12-19 09:18:24 +0100

    rendering a list of components + key value for each component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import SearchInput from "./SearchInput";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
 	state = { images: [] };
@@ -10,15 +11,16 @@ class App extends React.Component {
 		const response = await axios.get(
 			`https://pixabay.com/api/?key=29305409-8468ce75218a81b211a88e2b7&q=${entry}&image_type=photo`
 		);
-		// console.log(response);
 		this.setState({ images: response.data.hits });
+		// console.log(images);
 	}
 
 	render() {
 		return (
 			<div className='ui container' style={{ marginTop: "30px" }}>
 				<SearchInput onSearchSubmit={this.onSearchSubmit} />
-				We have {this.state.images.length} images
+				{/* We have {this.state.images.length} images */}
+				<ImageList images={this.state.images} />
 			</div>
 		);
 	}
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,1 +1,11 @@
 import React from "react";
+
+const ImageList = props => {
+	// console.log(props);
+	const images = props.images.map(image => {
+		return <img key={image.id} src={image.webformatURL} alt='searchImg' />;
+	});
+	return <div>{images}</div>;
+};
+
+export default ImageList;
